67b4e7494caab49eff02095c0fa31b1ea325b06a
Pavan <pavanb0403@gmail.com>
2025-12-13 15:22:04 +0530

    Made Theater also Use Cloudinary added Supress warnings

--- src/main/java/com/jsp/book_my_ticket/config/WebConfig.java
+++ src/main/java/com/jsp/book_my_ticket/config/WebConfig.java
@@ -12,4 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
 		String path = System.getProperty("user.dir") + "/uploads/";
 		registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + path);
 	}
-		}
+	
+}
