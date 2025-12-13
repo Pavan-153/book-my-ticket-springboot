44c4f2cfb4ca8fba68f94f7cc5f57c00e62e8964
Pavan <pavanb0403@gmail.com>
2025-12-13 15:39:49 +0530

    Fixed Issue with RedisTemplate bean

--- src/main/java/com/jsp/book_my_ticket/config/MyConfig.java
+++ src/main/java/com/jsp/book_my_ticket/config/MyConfig.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -20,15 +19,16 @@ public class MyConfig {
 		return new SecureRandom();
 	}
 
+	
+	@SuppressWarnings("removal")
 	@Bean
-	@SuppressWarnings("deprecation")
 	RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 		template.setHashKeySerializer(new StringRedisSerializer());
-		template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 		template.afterPropertiesSet();
 		return template;
 	}
